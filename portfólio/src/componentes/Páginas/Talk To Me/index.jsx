11566d6d89279e083b60181a9477e9e06b4ecec1
teteulf <theolefevre791@gmail.com>
2023-12-11 19:01:57 -0300

    acrescentando dialog no component "talk to me"

--- portfólio/src/componentes/Páginas/Talk To Me/index.jsx
+++ portfólio/src/componentes/Páginas/Talk To Me/index.jsx
@@ -14,9 +14,10 @@ const Alinhamento = styled.main`
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 38px;
+  gap: 30px;
   box-sizing: border-box;
   width: 70%;
+  margin-top: -10%;
   div {
     display: flex;
     gap: 20px;
@@ -41,19 +42,99 @@ const FormContainer = styled.form`
     height: 130px;
     padding: 12px 0px 82px 20px;
   }
+
+  .inputButton {
+    width: 10rem;
+    height: 3rem;
+    cursor: pointer;
+    border-radius: 30px;
+    border: 1px solid #05b40296;
+    padding: 12px -5px 12px 0px;
+    box-sizing: border-box;
+    background-color: transparent;
+    color: #ffffff8a;
+
+    &:hover {
+      background-color: #01012b;
+    }
+  }
+`;
+
+const Dialogue = styled.dialog`
+  position: absolute;
+  width: 200px;
+  height: 100px;
+  background-color: white;
+  text-align: center;
+
+  &::backdrop {
+    background-color: #03011acc;
+  }
+
+  .buttonDialog {
+    position: relative;
+    top: 70%;
+    right: 16%;
+    width: 80px;
+    height: 20px;
+  }
 `;
 
 export function TalkToMe() {
+  const modal = document.querySelector(".ModalDialogue");
   return (
-    <Alinhamento>
-      <FormContainer>
-        <div>
-          <input className="inputDefault" placeholder="name" />
-          <input type="email" className="inputDefault" placeholder="email" />
-        </div>
-        <input className="inputDefault inputSubject" placeholder="Subject" />
-        <input className="inputDefault inputMessage" placeholder="Message" />
-      </FormContainer>
-    </Alinhamento>
+    <>
+      <Alinhamento>
+        <FormContainer>
+          <h1
+            style={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              gap: "10px",
+              fontSize: "40px",
+            }}
+          >
+            Let's <strong style={{ color: "red" }}>Connect.</strong>
+          </h1>
+          <div>
+            <input className="inputDefault" placeholder="name" />
+            <input type="email" className="inputDefault" placeholder="email" />
+          </div>
+          <input className="inputDefault inputSubject" placeholder="Subject" />
+          <input className="inputDefault inputMessage" placeholder="Message" />
+          <input
+            type="button"
+            className="inputButton"
+            value={"Send"}
+            onClick={() => {
+              modal.showModal();
+            }}
+          />
+        </FormContainer>
+      </Alinhamento>
+      <Dialogue className="ModalDialogue">
+        Email enviado com sucesso!
+        <button
+          className="buttonDialog"
+          onClick={() => {
+            modal.close();
+          }}
+        >
+          Ok
+        </button>
+      </Dialogue>
+      <Dialogue>
+        Por favor preencha todos os campos.
+        <button
+          className="buttonDialog"
+          onClick={() => {
+            modal.close();
+          }}
+        >
+          Ok
+        </button>
+      </Dialogue>
+    </>
   );
 }
--- portfólio/src/componentes/Tipografia/index.jsx
+++ portfólio/src/componentes/Tipografia/index.jsx
@@ -1,12 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const TipografiaStyled = styled.div`
-  color: ${props => props.cor || '#da1414'};
-  font-size: ${props => props.tamanhoFonte || '40px'};
+  color: ${(props) => props.cor || "#da1414"};
+  font-size: ${(props) => props.tamanhoFonte || "40px"};
 `;
 
 function Tipografia({ cor, tamanhoFonte }) {
-  return <TipografiaStyled cor={cor} tamanhoFonte={tamanhoFonte}>.</TipografiaStyled>;
+  return (
+    <TipografiaStyled cor={cor} tamanhoFonte={tamanhoFonte}>
+      .
+    </TipografiaStyled>
+  );
 }
 
 export default Tipografia;
