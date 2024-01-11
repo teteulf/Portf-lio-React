eb3cd74faa5c45fa8faec4e34586dadc27b1008d
teteulf <theolefevre791@gmail.com>
2024-01-11 18:50:04 -0300

    refactor

--- src/componentes/Páginas/Timer/index.jsx
+++ src/componentes/Páginas/Timer/index.jsx
@@ -1,17 +1,3 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-export function Timer({ children }){
- const [isMounted, setIsMounted] = useState(false);
-
- useEffect(() => {
-   const timer = setTimeout(() => {
-     setIsMounted(true);
-   }, 1000);
-
-   return () => clearTimeout(timer);
- }, []);
-
- return isMounted ? children : null;
-};
-
-export default Transition;
+export function DelayTransition() {}
